2090a1dfe62d4be6537c5a65e98d7a7e6289dbe8
rgsk <guptarahul70322@gmail.com>
2022-05-08 02:07:03 +0530

    basic routes

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-print("hello")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def bubble_sort():
-    print('bubble sort is working')
+@app.get('/')
+def root():
+    return {"message": "Hello World changed 12312312"}
+
+
+@app.get('/login')
+def login():
+    return {"message": "Within a year"}
+
+
+posts = [{"id": 1, "text": "first post"}]
+
+
+@app.get('/posts')
+def get_posts():
+    return posts
