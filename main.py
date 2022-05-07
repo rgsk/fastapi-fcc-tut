bdc8b5edee9994df1d997934bc92106b73d5adb9
rgsk <guptarahul70322@gmail.com>
2022-05-08 03:24:28 +0530

    return status codes properly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -25,14 +26,39 @@ def get_posts():
     return posts
 
 
-@app.post('/posts')
+@app.get('/posts/latest')
+def get_latest_post():
+    if len(posts) > 0:
+        return posts[-1]
+
+
+@app.get('/posts/{id}')
+def get_post(id: int, response: Response):
+    for post in posts:
+        if post['id'] == id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f'no post found with id {id}')
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return {
+    #     'message': f'no post found with id {id}'
+    # }
+
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     created_post = post.dict()
     created_post['id'] = len(posts)
     posts.append(created_post)
+    # respose.status_code = status.HTTP_201_CREATED
     return created_post
 
 
-@app.delete('/posts')
-def delete_post():
+@app.patch('/posts/{id}')
+def update_post():
+    return {}
+
+
+@app.delete('/posts/{id}')
+def delete_post(id):
     return {}
